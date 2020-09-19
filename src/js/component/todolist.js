6fe76861be155a64e23b64bbfd5020b187686202
jdglez91 <jdglez1991@gmail.com>
2020-09-19 01:04:29 +0000

    incomplete

---
+++ src/js/component/todolist.js
@@ -0,0 +1,6 @@
+// 1. create an input field to collect task from the user 
+//2. Create arrays of object to store task
+//3. Create a function to save the value of the input field into the array
+//4. Add an Event lisenert to our input field , onChange will need to call a Function
+//5. Create a variable to store the value of input field.
+//6. The function from onChange event needs to set the hock variable with the current value of input.
